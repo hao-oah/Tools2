53ef49fb2ab04f3eb48d0ca4b4680dfcc8e05892
hao-oah <31794231+hao-oah@users.noreply.github.com>
2018-09-27 15:08:17 +0200

    yez

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@
 	var Wall = true;
 	var end_past_x;
 	var end_past_y;
-/*
+
 	function mousePressed() {
 			if(!!mouseX&!!mouseY){
 				end_past_x = Math.floor(cols*mouseX/windowWidth);
@@ -40,7 +40,7 @@
 				iter--;
 				generateNewBoard(); 
 			}
-	}*/
+	}
 
 
 	function generateNewBoard() {
@@ -200,7 +200,7 @@
 		var canvas = createCanvas(windowWidth, windowHeight);
 		w = (width / cols);
 		h = (height / rows);
-		/*canvas.mousePressed(redraw);*/
+		canvas.mousePressed(mousePressed);
 		generateNewBoard();
 
 	}
